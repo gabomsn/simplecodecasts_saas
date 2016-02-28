3dd714d3f5d1aca7b6c4a287a81dcf41283aadf6
Gabo Mess <gabm89@gmail.com>
2016-02-28 03:59:37 +0000

    Added flash alerts for sent messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,23 @@
 class ContactsController < ApplicationController
-    def new
-        @contact = Contact.new
+  def new
+    @contact = Contact.new
+  end
+  
+  def create
+    @contact = Contact.new(contact_params)
+    
+    if @contact.save
+      flash[:success] = "Message sent."
+      redirect_to new_contact_path
+    else
+      flash[:error] = "Error occured, message has not been sent."
+      redirect_to new_contact_path
+    end
+  end
+  
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
     end
     
-    def create
-    end
 end
